7f172b89e5e94f045000eb4cfc0fd17f9cbca706
trietphamskibidi <minhtrietpham23@gmail.com>
2026-05-28 16:55:49 -0500

    Refactor food and head classes, update main loop

--- Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
 #include <raylib.h>
 #include "obstacle.h"
-
+#include<deque>
 using namespace std;
-class food{
-    public:
-       void spawnrandom()
-       {
-        foodx=GetRandomValue(1,2);
-        foody= GetRandomValue(2,3);
-       }
-    
-    protected:
-    private: 
-        float foodx;
-        float foody;
-
-};
 int cellsize=20;
 int cellcount=40;
+class food
+{
+public:
+    Vector2 foodpos;
+    void Draw()
+    {
+        DrawRectangle((foodpos.x)*cellsize,(foodpos.y)*cellsize,cellsize,cellsize,WHITE);
+    }
+};
+class snake
+{
+public:
+  deque<Vector2> body;
+};
+class head
+{public:
+    Vector2 headpos;
+    void Draw()
+    {
+     DrawCircle((headpos.x)*cellsize,(headpos.y)*cellsize,10,RED);
+    }
+
+};
 
 int main () {
 
-   
-    int xcircle=400;
-    int ycircle=400;
-    int v_y=5;
-
+    food Food;
+    head Head;
+    Head.headpos.x=20;
+    Head.headpos.y=20;
+    float v_y=0.125;
+    
 
     cout << "Hello World" << endl;
 
     InitWindow(cellsize*cellcount,cellsize*cellcount, "My first RAYLIB program!");
     SetTargetFPS(60);
+    Food.foodpos.x=GetRandomValue(6,35);
+    Food.foodpos.y=GetRandomValue(6,35);
 
     while (WindowShouldClose() == false)
-    {
-        ycircle-=v_y;
+    {  
+
+        Head.headpos.y-=v_y;
         BeginDrawing();
         ClearBackground(BLACK);
-        Rectangle borderfake={100,100,600,600};
-        
+        Rectangle borderfake={100,100,620,620};
         DrawRectangleLinesEx(borderfake,20,WHITE); 
-        //drawing the border
-        DrawCircle(xcircle,ycircle,5.25,RED);
+        Head.Draw();
+        Food.Draw();
         EndDrawing();
 
     }
