5870928314fd5a93f03572d2a5ec16d0f1c7605e
trietphamskibidi <minhtrietpham23@gmail.com>
2026-06-03 22:02:49 -0500

    Movement handling
    Making the snake continously move, not based on the code, and adding if logic to legal the move

--- Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <raylib.h>
 #include "obstacle.h"
+#include "raymath.h"
 #include<deque>
 using namespace std;
 int cellsize=20;
 int cellcount=40;
 float countingtime=0;
-int movementtracking=0;
+Vector2 direction={0,-1};
+Vector2 pendingDirection={0,-1};
+
 class food
 {
 public:
@@ -16,43 +19,26 @@ public:
         DrawRectangle((foodpos.x)*cellsize,(foodpos.y)*cellsize,cellsize,cellsize,WHITE);
     }
 };
+
 class snake
 {
 public:
   float vy=1;
   deque<Vector2> snake={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
   void Draw()
-  { DrawRectangle(snake[0].x*cellsize,snake[0].y*cellsize,cellsize,cellsize, ORANGE);
+  { 
+    DrawRectangle(snake[0].x*cellsize,snake[0].y*cellsize,cellsize,cellsize, ORANGE);
     for (int i=1; i<snake.size();i++)
     {
-    int x=snake[i].x;
-    int y=snake[i].y;
-    DrawRectangle(x*cellsize,y*cellsize,cellsize,cellsize,ORANGE);
+        int x=snake[i].x;
+        int y=snake[i].y;
+        DrawRectangle(x*cellsize,y*cellsize,cellsize,cellsize,ORANGE);
     }
   };
-  void movingup()
-  {
+  void move()
+  { 
     snake.pop_back();
-    snake.push_front({snake[0].x,(snake[0].y)-1});
-    movementtracking=0;
-  };
-  void movingdown()
-  {
-    snake.pop_back();
-    snake.push_front({snake[0].x,(snake[0].y)+1});
-    movementtracking=1;
-  }
-  void movingleft()
-  {
-    snake.pop_back();
-    snake.push_front({(snake[0].x)-1,snake[0].y});
-    movementtracking=2;
-  }
-  void movingright()
-  {
-   snake.pop_back();
-    snake.push_front({(snake[0].x)+1,snake[0].y});
-    movementtracking=3;
+    snake.push_front(Vector2Add(snake[0],direction));
   }
 };
 
@@ -73,23 +59,19 @@ int main () {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        if (IsKeyPressed(KEY_W) and movementtracking !=1)
+        if (IsKeyPressed(KEY_W) && direction.y != 1  && pendingDirection.y != 1)  pendingDirection = {0,-1};
+        if (IsKeyPressed(KEY_A) && direction.x != 1  && pendingDirection.x != 1)  pendingDirection = {-1,0};
+        if (IsKeyPressed(KEY_S) && direction.y != -1 && pendingDirection.y != -1) pendingDirection = {0, 1};
+        if (IsKeyPressed(KEY_D) && direction.x != -1 && pendingDirection.x != -1) pendingDirection = {1, 0};
+
+        if (GetTime()-countingtime>=0.25)
         {
-            body.movingup();
+            countingtime=GetTime();
+            direction=pendingDirection;
+            body.move();
         }
-        if(IsKeyPressed(KEY_A) and movementtracking!=3)
-        {
-            body.movingleft();
-        }
-        if(IsKeyPressed(KEY_S) and movementtracking!=0)
-        {
-            body.movingdown();
-        }
-        if(IsKeyPressed(KEY_D) and movementtracking!=2)
-        {
-            body.movingright();
-        }
-    body.Draw();
+        
+        body.Draw();
         Rectangle borderfake={100,100,620,620};
         DrawRectangleLinesEx(borderfake,20,WHITE); 
         Food.Draw();
