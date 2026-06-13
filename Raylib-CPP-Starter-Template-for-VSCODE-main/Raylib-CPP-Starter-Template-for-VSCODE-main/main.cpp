68696ac322e52ba2ed1d5f0c994c8f1c2759ac3f
trietphamskibidi <minhtrietpham23@gmail.com>
2026-06-12 23:34:56 -0500

    Changing a bunch of game logic, making the game more oop
    Refactor snake and food classes, update game stages and logic.

--- Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -4,11 +4,72 @@
 #include "raymath.h"
 #include<deque>
 using namespace std;
+class snake;
+class food;
+class gamestage;
+enum differentstages;
 int cellsize=20;
 int cellcount=40;
 float countingtime=0;
 Vector2 direction={0,-1};
 Vector2 pendingDirection={0,-1};
+differentstages stages=gameinitialize;
+enum differentstages
+{
+ gameinitialize,
+ gamerunning,
+ gamepausing,
+ gameover,
+ gamerestart,
+}
+
+class snake
+{
+public:
+  float vy=1;
+  deque<Vector2> bodysegment={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
+  void Draw()
+  { 
+    DrawRectangle(bodysegment[0].x*cellsize,bodysegment[0].y*cellsize,cellsize,cellsize, ORANGE);
+    for (int i=1; i<bodysegment.size();i++)
+    {
+        int x=bodysegment[i].x;
+        int y=bodysegment[i].y;
+        DrawRectangle(x*cellsize,y*cellsize,cellsize,cellsize,ORANGE);
+    }
+  };
+
+  void move()
+  { 
+    bodysegment.pop_back();
+    bodysegment.push_front(Vector2Add(bodysegment[0],direction));
+  }
+  void grow()
+{
+    bodysegment.push_back(Vector2Subtract(bodysegment.back(),direction));
+}
+  void check()
+
+  {
+    for (int i=1;i<bodysegment.size(); i++)
+     {
+       if (bodysegment[0].x==bodysegment[i].x and bodysegment[0].y==bodysegment[i].y)
+        {
+            game
+    
+        }
+     }
+     if(bodysegment[0].x>34 || bodysegment[0].x<6||bodysegment[0].y<6||bodysegment[0].y>34)
+     {
+       bodysegment={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
+       pendingDirection={0,-1};
+    direction={0,-1};
+     }
+  }
+  
+
+  
+};
 
 class food
 {
@@ -29,72 +90,18 @@ public:
         foodpos.y=GetRandomValue(6,34);
     }
 };
-
-class snake
+class gamestage
 {
-public:
-  float vy=1;
-  deque<Vector2> snake={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
-  void Draw()
-  { 
-    DrawRectangle(snake[0].x*cellsize,snake[0].y*cellsize,cellsize,cellsize, ORANGE);
-    for (int i=1; i<snake.size();i++)
-    {
-        int x=snake[i].x;
-        int y=snake[i].y;
-        DrawRectangle(x*cellsize,y*cellsize,cellsize,cellsize,ORANGE);
-    }
-  };
-
-  void move()
-  { 
-    snake.pop_back();
-    snake.push_front(Vector2Add(snake[0],direction));
-  }
-  void grow()
-{
-    snake.push_back(Vector2Subtract(snake.back(),direction));
-}
-  void check()
-
-  {
-    for (int i=1;i<snake.size(); i++)
+    public:
+     Rectangle label={150,150, 520, 170};
+     void gameinitialize()
      {
-       if (snake[0].x==snake[i].x and snake[0].y==snake[i].y)
-        {
-            snake={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
-            pendingDirection={0,-1};
-    direction={0,-1};
-    
-        }
+      DrawRectangleLinesEx(label, 20, PINK);
+      DrawText("Snake Game Project", 190,190, 35, WHITE);
+      DrawText("Press R to start playing", 300, 450, 25, ORANGE);
      }
-     if(snake[0].x>34 || snake[0].x<6||snake[0].y<6||snake[0].y>34)
-     {
-       snake={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
-       pendingDirection={0,-1};
-    direction={0,-1};
-     }
-  }
-  
-
-  
-};
-
-int main () {
-    
-    snake body;
-    food Food;
-
-    cout << "Hello World" << endl;
-
-    InitWindow(cellsize*cellcount,cellsize*cellcount, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (WindowShouldClose() == false)
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
+     void gamerunning(Vector2 &direction, Vector2&pendingDirection, snake &body, food &Food)
+     {  ClearBackground(BLACK);
         if (IsKeyPressed(KEY_W) && direction.y != 1  && pendingDirection.y != 1)  pendingDirection = {0,-1};
         if (IsKeyPressed(KEY_A) && direction.x != 1  && pendingDirection.x != 1)  pendingDirection = {-1,0};
         if (IsKeyPressed(KEY_S) && direction.y != -1 && pendingDirection.y != -1) pendingDirection = {0, 1};
@@ -104,18 +111,74 @@ int main () {
             countingtime=GetTime();
             direction=pendingDirection;
             body.move();
-            if (body.snake[0].x == Food.foodpos.x && body.snake[0].y == Food.foodpos.y)
+            if (body.bodysegment[0].x == Food.foodpos.x && body.bodysegment[0].y == Food.foodpos.y)
             {
                 body.grow();
                 Food.geteaten();
             }
         }
         body.check();
-        
         body.Draw();
-        Rectangle borderfake={100,100,620,620};
-        DrawRectangleLinesEx(borderfake,20,WHITE); 
         Food.Draw();
+     }
+     void gameover()
+     {
+        DrawText("Press space to start again", 300, 450, 25, ORANGE);
+     }
+     void gamepause()
+     {
+        DrawText("click anywhereto start again", 300, 450,25, ORANGE);
+     }
+};
+int main () {
+    snake body;
+    food Food;
+    gamestage Stage;
+    InitWindow(cellsize*cellcount,cellsize*cellcount, "My first RAYLIB program!");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        Rectangle borderfake={100,100,620,620};
+        DrawRectangleLinesEx(borderfake,20,WHITE);
+        if (gamestage==gameinitialize)
+        {
+           Stage.gameinitialize();
+           if(IsKeyPressed(KEY_R))
+           {
+            gamestage=gamerunning;
+           }
+        }
+        if(gamestage==gamerunning)
+        {
+            Stage.gamerunning(direction,pendingDirection,body,Food);
+            if(IsKeyPressed(KEY_ESCAPE))
+            {
+                gamestage=gamepausing;
+            }
+            
+        }
+        if(gamestage==gamepausing)
+        {
+            Stage.gamepause();
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                gamestage=gamerunning;
+            }
+        }
+        if(gamestage==gameover)
+        {
+            Stage.gameover();
+            if(IsKeyPressed(KEY_SPACE))
+            {
+                Stage.gameinitialize();
+            }
+        }
+
+        
+
         EndDrawing();
     }
 }
