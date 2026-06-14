bd2a3e809682a4af47fffcd567e092ed09fe065f
trietphamskibidi <minhtrietpham23@gmail.com>
2026-06-14 17:14:52 -0500

    game finishing
    changing a little bit of game logic and finishing up the small game

--- Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -7,13 +7,11 @@ using namespace std;
 class snake;
 class food;
 class gamestage;
-enum differentstages;
 int cellsize=20;
 int cellcount=40;
 float countingtime=0;
 Vector2 direction={0,-1};
 Vector2 pendingDirection={0,-1};
-differentstages stages=gameinitialize;
 enum differentstages
 {
  gameinitialize,
@@ -21,8 +19,27 @@ enum differentstages
  gamepausing,
  gameover,
  gamerestart,
-}
-
+};
+differentstages stage=gameinitialize;
+class food
+{
+public:
+    Vector2 foodpos;
+    food()
+    {
+        foodpos.x=GetRandomValue(6,35);
+        foodpos.y=GetRandomValue(6,35);
+    }
+    void Draw()
+    {
+        DrawRectangle((foodpos.x)*cellsize,(foodpos.y)*cellsize,cellsize,cellsize,WHITE);
+    }
+    void geteaten()
+    {
+        foodpos.x=GetRandomValue(6,34);
+        foodpos.y=GetRandomValue(6,34);
+    }
+};
 class snake
 {
 public:
@@ -49,57 +66,36 @@ public:
     bodysegment.push_back(Vector2Subtract(bodysegment.back(),direction));
 }
   void check()
-
-  {
+{
     for (int i=1;i<bodysegment.size(); i++)
      {
        if (bodysegment[0].x==bodysegment[i].x and bodysegment[0].y==bodysegment[i].y)
         {
-            game
-    
+            stage=gameover;
         }
      }
      if(bodysegment[0].x>34 || bodysegment[0].x<6||bodysegment[0].y<6||bodysegment[0].y>34)
      {
-       bodysegment={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
-       pendingDirection={0,-1};
-    direction={0,-1};
+       stage=gameover;
      }
-  }
+}
   
 
   
-};
-
-class food
-{
-public:
-    Vector2 foodpos;
-    food()
-    {
-        foodpos.x=GetRandomValue(6,35);
-        foodpos.y=GetRandomValue(6,35);
-    }
-    void Draw()
-    {
-        DrawRectangle((foodpos.x)*cellsize,(foodpos.y)*cellsize,cellsize,cellsize,WHITE);
-    }
-    void geteaten()
-    {
-        foodpos.x=GetRandomValue(6,34);
-        foodpos.y=GetRandomValue(6,34);
-    }
 };
 class gamestage
 {
     public:
      Rectangle label={150,150, 520, 170};
-     void gameinitialize()
+     void gameinitialize(snake &body)
      {
       DrawRectangleLinesEx(label, 20, PINK);
       DrawText("Snake Game Project", 190,190, 35, WHITE);
       DrawText("Press R to start playing", 300, 450, 25, ORANGE);
+     body.bodysegment={Vector2{20,21},Vector2{20,22},Vector2{20,23}};
+     pendingDirection={0,-1};
      }
+     
      void gamerunning(Vector2 &direction, Vector2&pendingDirection, snake &body, food &Food)
      {  ClearBackground(BLACK);
         if (IsKeyPressed(KEY_W) && direction.y != 1  && pendingDirection.y != 1)  pendingDirection = {0,-1};
@@ -127,9 +123,9 @@ class gamestage
      }
      void gamepause()
      {
-        DrawText("click anywhereto start again", 300, 450,25, ORANGE);
+        DrawText("click anywhere to start again", 300, 450,25, ORANGE);
      }
-};
+    };
 int main () {
     snake body;
     food Food;
@@ -143,37 +139,38 @@ int main () {
         ClearBackground(BLACK);
         Rectangle borderfake={100,100,620,620};
         DrawRectangleLinesEx(borderfake,20,WHITE);
-        if (gamestage==gameinitialize)
+        if (stage==gameinitialize)
         {
-           Stage.gameinitialize();
+           Stage.gameinitialize(body);
+
            if(IsKeyPressed(KEY_R))
            {
-            gamestage=gamerunning;
+            stage=gamerunning;
            }
         }
-        if(gamestage==gamerunning)
+        if(stage==gamerunning)
         {
             Stage.gamerunning(direction,pendingDirection,body,Food);
-            if(IsKeyPressed(KEY_ESCAPE))
+            if(IsKeyPressed(KEY_M))
             {
-                gamestage=gamepausing;
+                stage=gamepausing;
             }
             
         }
-        if(gamestage==gamepausing)
+        if(stage==gamepausing)
         {
             Stage.gamepause();
             if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
             {
-                gamestage=gamerunning;
+                stage=gamerunning;
             }
         }
-        if(gamestage==gameover)
+        if(stage==gameover)
         {
             Stage.gameover();
             if(IsKeyPressed(KEY_SPACE))
             {
-                Stage.gameinitialize();
+                stage=gameinitialize;
             }
         }
 
